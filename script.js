fb22aae2f39c5ecc18c6fae028d7999648a4784f
Piero Alexsis Rosales Vivanco <101838439+PieroRV-Dev@users.noreply.github.com>
2023-12-02 18:23:44 -0500

    first

---
+++ script.js
@@ -0,0 +1,32 @@
+
+onload = () => {
+    const c = setTimeout(() => {
+
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+      const palabras = ["Hola Sami :D", 
+      "Esto es para ti <3", 
+      "No quiero que te sientas ni desanimada ni triste",
+      "Yo siempre voy estar a tu lado apoyandote",
+      "Confio en ti, y eres capaz de realizar cualquier cosa",
+      "Tu sonrisa ilumina mi día, y por eso eres especial para mí."];
+
+      const elementoCentro = document.getElementById("centro");
+      let indicePalabra = 0;
+    
+      // Función para cambiar la palabra cada 10 segundos y reiniciar el audio
+      function cambiarPalabra() {
+        elementoCentro.textContent = palabras[indicePalabra];
+
+        indicePalabra = (indicePalabra + 1) % palabras.length;
+
+      }
+      // Cambiar la palabra inicialmente y establecer el intervalo
+      cambiarPalabra();
+    
+      setInterval(cambiarPalabra, 6000);
+    }, 1000);
+  };
+  
+
+ 
